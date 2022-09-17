1e90e0167dba5e0ee8cbb62abfbfb45d7c7a161b
k <lingweikong10@gmail.com>
2022-09-17 12:17:33 -0500

    Add selection inputs.

--- index.js
+++ index.js
@@ -1,42 +1,99 @@
-makeBoard(16 * 16);
+//size input
+const sizeInput = document.querySelector("#size-input");
+let row = sizeInput.value;
+const span = document.querySelector(".num-row");
+span.textContent = row + " * " + row;
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  const size = prompt("how large?");
+sizeInput.addEventListener("change", (e) => {
+  console.log("new row no", e.target.value);
+  row = e.target.value;
+  span.textContent = row + " * " + row;
   clearBoard();
-  makeBoard(size);
+  makeBoard();
 });
 
-function makeBoard(size) {
-  const row = (col = Math.sqrt(size));
+// makeBoard(row, row);
+function makeBoard() {
   const board = document.querySelector(".board");
-  for (let i = 0; i < row; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+  board.setAttribute("draggable", false);
 
-    for (let j = 0; j < col; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
+  board.setAttribute(
+    "style",
+    `grid-template: repeat(${row}, 1fr)/repeat(${row}, 1fr)`
+  );
+  for (let i = 0; i < row * row; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
 
-      cell.addEventListener("mousedown", drawPixel);
-      cell.addEventListener("mouseenter", drawPixel);
-      row.appendChild(cell);
-    }
-
-    board.appendChild(row);
+    cell.addEventListener("mousedown", drawPixel);
+    cell.addEventListener("mouseenter", drawPixel);
+    board.appendChild(cell);
   }
 }
+makeBoard();
 
 function clearBoard() {
-  const board = document.querySelector(".board");
-  while (board.hasChildNodes()) {
-    board.removeChild(board.firstChild);
+  const main = document.querySelector(".main");
+  main.removeChild(main.lastElementChild);
+
+  const board = document.createElement("div");
+  board.classList.add("board");
+  main.appendChild(board);
+}
+
+//color input
+const colorInput = document.querySelector(".color-input");
+let color = colorInput.value;
+colorInput.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  color = colorInput.value;
+});
+
+let mode = "draw";
+
+function drawPixel(e) {
+  // console.log(e);
+  if (e.buttons >= 1) {
+    if (mode == "draw") {
+      e.target.style.backgroundColor = color;
+    } else if (mode == "erase") {
+      e.target.style.backgroundColor = "transparent";
+    } else if (mode == "rainbow") {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
   }
 }
 
-function drawPixel(e) {
-  console.log(e);
-  if (e.buttons >= 1) {
-    e.target.classList.add("change-color");
+const eraser = document.querySelector(".eraser");
+const rainbowMode = document.querySelector(".rainbow");
+eraser.addEventListener("click", () => {
+  if (mode == "erase") {
+    mode = "draw";
+    eraser.classList.remove("eraser-on");
+  } else {
+    mode = "erase";
+    eraser.classList.add("eraser-on");
+    rainbowMode.classList.remove("rainbow-on");
   }
-}
+});
+
+rainbowMode.addEventListener("click", () => {
+  if (mode == "rainbow") {
+    mode = "draw";
+    eraser.classList.remove("eraser-on");
+  } else {
+    mode = "rainbow";
+
+    rainbowMode.classList.add("rainbow-on");
+    eraser.classList.remove("eraser-on");
+  }
+});
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+  clearBoard();
+  makeBoard();
+});
